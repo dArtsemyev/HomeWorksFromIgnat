55f20e8b421fcd4cb9e9776b38bc209257a5a825
dArtsemyev <d.artsemyev@gmail.com>
2021-06-08 23:59:35 +0300

    HW#2 is done

--- src/p2-homeworks/h2/Affair.tsx
+++ src/p2-homeworks/h2/Affair.tsx
@@ -1,19 +1,36 @@
 import React from 'react'
+import s from "./Affair.module.css"
+import {AffairType} from "./HW2";
 
 type AffairPropsType = {
     // key не нужно типизировать
-    affair: any // need to fix any
-    deleteAffairCallback: any // need to fix any
+    id: number
+    name: string
+    priority: string
+    affair: AffairType // need to fix any
+    deleteAffairCallback: (id: number) => void // need to fix any
 }
 
 function Affair(props: AffairPropsType) {
-    const deleteCallback = () => {}// need to fix
+
+    const deleteCallback = () => { // need to fix
+        props.deleteAffairCallback(props.id)
+    }
 
     return (
-        <div>
-            // show some text
-
-            <button onClick={deleteCallback}>X</button>
+        <div className={s.affairInfo}>
+            <div className={s.item}>
+                {props.id}
+            </div>
+            <div className={s.item}>
+                {props.name}
+            </div>
+            <div className={`${s.item} + ${props.priority === "high" ? s.high : (props.priority === "middle" ? s.middle : s.low)}`}>
+                {props.priority}
+            </div>
+            <div>
+                <button onClick={deleteCallback}>X</button>
+            </div>
         </div>
     )
 }
